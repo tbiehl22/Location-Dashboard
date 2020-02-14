717c2e3e1fcc0e02ae6a333cd204c48ae9933ae0
scruffythejanitor <pemberton5150@gmail.com>
2020-02-13 19:02:01 -0700

    updated javascript

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,20 @@
 $(document).ready(function () {
-    
-    
+
+    var city = $("#city-input").val();
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial" +
+        "&api_key=c2fa28678b1d4b3ad38208513c930f6b";
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).done(function (data) {
+        console.log(data);
+    })
+        var time
+        setInterval(function () {
+            time = moment().format('LTS');
+        }, 1000)
+        console.log(time);
 
 
-});
+
+    });
