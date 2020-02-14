f0e180f7817f72db816e1df458175196c0bd0e7f
scruffythejanitor <pemberton5150@gmail.com>
2020-02-13 20:21:09 -0700

    added some variables for weather app

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,7 @@
 $(document).ready(function () {
     
     // var city = $("#city-input").val();
+    var degreeSymbol = String.fromCharCode(176)
     var city = "farmington, utah";
     var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=c2fa28678b1d4b3ad38208513c930f6b";
    
@@ -8,17 +9,19 @@ $(document).ready(function () {
       url: queryURL,
       method: "GET"
     }).then(function(response) {
+      console.log(response);
+      
       var city = response.name
-      console.log(city);
       var temp = response.main.temp
-      console.log(temp);
       var fahrenheitDec = (temp - 273.15) * 1.80 + 32;
       var fahrenheit = Math.floor(fahrenheitDec)
-      console.log(fahrenheit);
+      var icon = response.weather.icon
+      console.log(icon);
       
-      console.log(queryURL);
-      console.log("this is working");
-        
+      $(".weather").text(fahrenheit + degreeSymbol)
+      $(".weather").text(fahrenheit + degreeSymbol)
+      $(".weather").text(fahrenheit + degreeSymbol)
+      $(".weather").text(fahrenheit + degreeSymbol)
       });
 
         // var time
