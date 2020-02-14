013c1ae358bf334e1bbf33f1f215c0c69aa19071
scruffythejanitor <pemberton5150@gmail.com>
2020-02-13 19:27:49 -0700

    trying to JS working

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,19 +1,23 @@
 $(document).ready(function () {
-
-    var city = $("#city-input").val();
-    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial" +
-        "&api_key=c2fa28678b1d4b3ad38208513c930f6b";
+    
+    // var city = $("#city-input").val();
+    var city = "farmington";
+    var queryURL = "https://www.api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=c2fa28678b1d4b3ad38208513c930f6b";
+   
     $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).done(function (data) {
-        console.log(data);
-    })
-        var time
-        setInterval(function () {
-            time = moment().format('LTS');
-        }, 1000)
-        console.log(time);
+      url: queryURL,
+      method: "GET"
+    }).then(function(response) {
+      console.log(queryURL);
+      console.log("this is working");
+        
+      });
+
+        // var time
+        // setInterval(function () {
+        //     time = moment().format('LTS');
+        // }, 1000)
+        // console.log(time);
 
 
 
