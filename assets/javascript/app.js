a55c3416e021e1c845412a5a7a8ec06598980919
scruffythejanitor <pemberton5150@gmail.com>
2020-02-13 19:40:59 -0700

    got query url working

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,13 +1,21 @@
 $(document).ready(function () {
     
     // var city = $("#city-input").val();
-    var city = "farmington";
-    var queryURL = "https://www.api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=c2fa28678b1d4b3ad38208513c930f6b";
+    var city = "farmington, utah";
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=c2fa28678b1d4b3ad38208513c930f6b";
    
     $.ajax({
       url: queryURL,
       method: "GET"
     }).then(function(response) {
+      var city = response.name
+      console.log(city);
+      var temp = response.main.temp
+      console.log(temp);
+      var fahrenheitDec = (temp - 273.15) * 1.80 + 32;
+      var fahrenheit = Math.floor(fahrenheitDec)
+      console.log(fahrenheit);
+      
       console.log(queryURL);
       console.log("this is working");
         
