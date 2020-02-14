c2786f31d0cf9e6b2826b9ffcc0b8f886d7ea468
scruffythejanitor <pemberton5150@gmail.com>
2020-02-13 21:48:08 -0700

    added weather fuctionality to js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,35 +1,46 @@
 $(document).ready(function () {
-    
-    // var city = $("#city-input").val();
-    var degreeSymbol = String.fromCharCode(176)
-    var city = "farmington, utah";
-    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=c2fa28678b1d4b3ad38208513c930f6b";
-   
-    $.ajax({
-      url: queryURL,
-      method: "GET"
-    }).then(function(response) {
-      console.log(response);
-      
-      var city = response.name
-      var temp = response.main.temp
-      var fahrenheitDec = (temp - 273.15) * 1.80 + 32;
-      var fahrenheit = Math.floor(fahrenheitDec)
-      var icon = response.weather.icon
-      console.log(icon);
-      
-      $(".weather").text(fahrenheit + degreeSymbol)
-      $(".weather").text(fahrenheit + degreeSymbol)
-      $(".weather").text(fahrenheit + degreeSymbol)
-      $(".weather").text(fahrenheit + degreeSymbol)
-      });
-
-        // var time
-        // setInterval(function () {
-        //     time = moment().format('LTS');
-        // }, 1000)
-        // console.log(time);
 
 
+  var degreeSymbol = String.fromCharCode(176)
+  var city
+  var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=c2fa28678b1d4b3ad38208513c930f6b";
 
-    });
+  console.log(city);
+  
+  $('#submitBtn').on('click', function(){
+    event.preventDefault()
+    city = $('#city-input').val();
+    console.log(city);
+  });
+
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function (response) {
+
+    var condition = response.weather[0].main;
+    var city = response.name;
+    var temp = response.main.temp;
+    var feelsLike = response.main.feels_like;
+    var windSpeed = response.wind.speed * 2.237;
+
+    function fahrenheitConverter(something) {
+      return Math.floor((something - 273.15) * 1.80 + 32)
+    };
+
+    $("#city").html(city)
+    $(".weather").html("<div class=\"degrees\">Temp: " + fahrenheitConverter(temp) + degreeSymbol + "f</div>")
+    $(".weather").append("<div class=\"feels-like\">Feels Like: " + fahrenheitConverter(feelsLike) + degreeSymbol + "</div>")
+    $(".weather").append("<div class=\"condition\">Condition: " + condition + "</div>")
+    $(".weather").append("<div class=\"wind-speed\"> Wind Speed: " + Math.round(windSpeed) + "mph</div>")
+  });
+
+  // Time Updater
+  var currentTime = Date.now();
+  setInterval(function () {
+    var formattedTime = moment.unix(currentTime / 1000).format('LT');
+    currentTime = currentTime + 1000;
+    $('#time').text(formattedTime)
+  }, 1000);
+
+});
